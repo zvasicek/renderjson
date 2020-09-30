1f1da6a04489e6ad9b17a4279dadbca0da1d181a
Zdenek Vasicek <37452722+zvasicek@users.noreply.github.com>
2020-09-30 12:38:10 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import pathlib
 
 setup(
     name='renderjson',  # Required
-    version='0.1.0',  # Required
+    version='0.1.1',  # Required
     description='Rendering JSON within IPython Notebook',  # Optional
 #    long_description=long_description,  # Optional
 #    long_description_content_type='text/markdown',  # Optional (see note above)
