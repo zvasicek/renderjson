8b9fd9d423fc4bbf3360154134ee77f214caa38f
Zdenek Vasicek <37452722+zvasicek@users.noreply.github.com>
2021-05-01 13:51:51 +0200

    Update __init__.py

--- renderjson/__init__.py
+++ renderjson/__init__.py
@@ -2,7 +2,7 @@ import json, uuid
 from IPython.display import display_javascript, display_html, display
 
 class RenderJSON(object):
-    def __init__(self, json_data, bg_color="#303030",text_color="#888"):
+    def __init__(self, json_data, show_to_level=1, bg_color="#303030",text_color="#888"):
         if isinstance(json_data, dict):
             self.json_str = json.dumps(json_data)
         else:
@@ -10,6 +10,7 @@ class RenderJSON(object):
         self.uuid = str(uuid.uuid4())
         self.bg_color = bg_color
         self.text_color = text_color
+        self.show_to_level = show_to_level
 
     def _ipython_display_(self):
         display_html("""<style>
@@ -31,7 +32,7 @@ class RenderJSON(object):
         require(["https://rawgit.com/caldwell/renderjson/master/renderjson.js"], function(r) {
             var node = document.getElementById('%s');
             node.innerHTML = ''; // vola se 2x neznamo proc
-            node.appendChild(renderjson.set_icons('\u25BA', '\u25BC').set_show_to_level(1)(%s));
+            node.appendChild(renderjson.set_icons('\u25BA', '\u25BC').set_show_to_level(%s)(%s));
             return true;
         });
-        """ % (self.uuid, self.json_str), raw=True)
+        """ % (self.uuid, self.show_to_level, self.json_str), raw=True)
